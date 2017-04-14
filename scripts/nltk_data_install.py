59af794312a3d03594af8e78775728616e201baa
Zera <mattzera@gmail.com>
2017-04-13 17:50:27 -0700

    changed nltk-data install

--- scripts/nltk_data_install.py
+++ scripts/nltk_data_install.py
@@ -5,7 +5,7 @@ directory = os.path.dirname(os.path.abspath(__file__))
 import nltk
 
 # Download the vader lexicon
-#nltk.download('vader_lexicon', directory+"/nltk_data")
-#nltk.download('all-corpora', directory+"/nltk_data")
+nltk.download('vader_lexicon', directory+"/nltk_data")
+nltk.download('all-corpora', directory+"/nltk_data")
 
-nltk.download()
+#nltk.download()
